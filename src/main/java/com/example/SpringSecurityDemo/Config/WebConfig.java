8bad3ccf17aa5b541efce01c17fc59412338a007
kamal karim <karim@example.com>
2025-01-22 09:27:57 +0100

    TAS-4 <Gestion des Tâches>

--- src/main/java/com/example/SpringSecurityDemo/Config/WebConfig.java
+++ src/main/java/com/example/SpringSecurityDemo/Config/WebConfig.java
@@ -27,6 +27,23 @@ public class WebConfig {
         };
     }
 
+//    @Bean
+//    public WebMvcConfigurer corsConfigurer() {
+//        return new WebMvcConfigurer() {
+//            @Override
+//            public void addCorsMappings(CorsRegistry registry) {
+//                registry.addMapping("/**")
+//                        .allowedOrigins("http://localhost:8082") // Frontend Angular (Docker)
+//                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+//                        .allowedHeaders("Content-Type", "Authorization", "X-Requested-With") // En-têtes spécifiques
+//                        .allowCredentials(true)
+//                        .maxAge(3600); // Permet de mettre en cache la réponse OPTIONS pendant 1 heure
+//            }
+//        };
+//    }
+
+
+
     @Bean
     public CorsFilter corsFilter() {
         CorsConfiguration corsConfiguration = new CorsConfiguration();
@@ -40,4 +57,20 @@ public class WebConfig {
 
         return new CorsFilter(source);
     }
+
+//    @Bean
+//    public CorsFilter corsFilter() {
+//        CorsConfiguration corsConfiguration = new CorsConfiguration();
+//        corsConfiguration.setAllowedOrigins(Arrays.asList("http://localhost:8082"));
+//        corsConfiguration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+//        corsConfiguration.setAllowedHeaders(Arrays.asList("Content-Type", "Authorization", "X-Requested-With"));
+//        corsConfiguration.setAllowCredentials(true);
+//        corsConfiguration.setMaxAge(3600L);
+//
+//        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+//        source.registerCorsConfiguration("/**", corsConfiguration);
+//
+//        return new CorsFilter(source);
+//    }
+
 }
